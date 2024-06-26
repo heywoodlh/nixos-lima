ca7ea330969882576f49773ecf888a6879209d2b
Spencer Heywood <github@heywoodlh.io>
2024-06-25 23:56:25 -0600

    added runVm output

--- flake.nix
+++ flake.nix
@@ -7,28 +7,58 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, flake-utils, nixos-generators, ... }@attrs: 
+  outputs = { self, nixpkgs, flake-utils, nixos-generators, ... }@attrs:
     # Create system-specific outputs for lima systems
     let
       ful = flake-utils.lib;
     in
-    ful.eachSystem [ ful.system.x86_64-linux ful.system.aarch64-linux ] (system:
+    ful.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        # Determine arch for building Linux system
+        # i.e. if on MacOS where system will not equal aarch64-linux
+        arch = if pkgs.lib.hasInfix "aarch64" "${system}" then "aarch64" else "x86_64";
+        linuxSystem = "${arch}-linux";
+        nixosImage = nixos-generators.nixosGenerate {
+          pkgs = import nixpkgs { system = linuxSystem; };
+          modules = [
+            ./lima.nix
+          ];
+          format = "raw-efi";
+        };
+        nixosYaml = pkgs.writeText "nixos.yaml" ''
+          arch: "${arch}"
+          images:
+            - location: "${nixosImage}/nixos.img"
+              arch: "${arch}"
+          cpus: 2
+          mounts:
+          - location: "~"
+            writable: true
+            9p:
+              # Try choosing "mmap" or "none" if you see a stability issue with the default "fscache".
+              cache: "mmap"
+          - location: "/tmp/lima"
+            writable: true
+            9p:
+              cache: "mmap"
+          mountType: "9p"
+          containerd:
+            system: false
+            user: false
+        '';
       in
       {
-        packages = {
-          img = nixos-generators.nixosGenerate {
-            inherit pkgs;
-            modules = [
-              ./lima.nix
-            ];
-            format = "raw-efi";
-          };
+        packages = rec {
+          img = nixosImage;
+          runVm = pkgs.writeShellScriptBin "nixos.sh" ''
+            ${pkgs.lima}/bin/limactl start --name=default ${nixosYaml}
+          '';
+          default = img;
         };
-      }) // { 
+      }) // {
         nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux"; # doesn't play nice with each system :shrug:
+          system = "aarch64-linux";
           specialArgs = attrs;
           modules = [
             ./lima.nix
